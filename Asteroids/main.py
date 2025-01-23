334ac4785760c5cd56d8e3befb444ed6e7877256
barronbytes <jonathan.m.barron@gmail.com>
2025-01-22 23:19:00 -0500

    circleshape: extend Sprite class

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -5,6 +5,8 @@ def main():
     # pygame setup
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 # refers to delta time
     running = True
 
     while running:
@@ -20,11 +22,7 @@ def main():
         # update display to show changes
         pygame.display.flip()
 
-
-
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
