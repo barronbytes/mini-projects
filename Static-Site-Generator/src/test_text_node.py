64885ba33f51c8c2e3da3f2e561ccb591fe6fa0b
barronbytes <jonathan.m.barron@gmail.com>
2025-02-02 12:57:58 -0500

    text_node: created to_leaf_node() method

--- Static-Site-Generator/src/test_text_node.py
+++ Static-Site-Generator/src/test_text_node.py
@@ -5,18 +5,43 @@ from text_node import TextType, TextNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        tn1 = TextNode("Hello world.", TextType.BOLD, "https://www.google.com/")
-        tn2 = TextNode("Hello world.", TextType.BOLD, "https://www.google.com/")
+        tn1 = TextNode("Hello world.", TextType.BOLD)
+        tn2 = TextNode("Hello world.", TextType.BOLD)
         self.assertEqual(tn1, tn2)
 
     def test_not_eq(self):
         tn1 = TextNode("Hello world.", TextType.BOLD)
-        tn2 = TextNode("Hello world.", TextType.CODE)
+        tn2 = TextNode("Hello world.", TextType.ITALIC)
         self.assertNotEqual(tn1, tn2)
 
-    def test_url_default_None(self):
-        tn1 = TextNode("Hello world.", TextType.BOLD)
-        self.assertIsNone(tn1.url)
+    def test_props_default_None(self):
+        tn = TextNode("Hello world.", TextType.BOLD)
+        self.assertIsNone(tn.props)
+
+    def test_tn_to_html_text(self):
+        node = TextNode("Hello world.", TextType.TEXT)
+        expected_html = "Hello world."
+        self.assertEqual(node.to_leaf_node().to_html(), expected_html)
+
+    def test_tn_to_html_bold(self):
+        node = TextNode("Hello world.", TextType.BOLD, {"class":"fw-900"})
+        expected_html = "<b class=\"fw-900\">Hello world.</b>"
+        self.assertEqual(node.to_leaf_node().to_html(), expected_html)
+
+    def test_tn_to_html_italic(self):
+        node = TextNode("Hello world.", TextType.ITALIC, {"class":"fw-900"})
+        expected_html = "<i class=\"fw-900\">Hello world.</i>"
+        self.assertEqual(node.to_leaf_node().to_html(), expected_html)
+
+    def test_tn_to_html_link(self):
+        node = TextNode("Boot Dev", TextType.LINK, "https://www.boot.dev")
+        expected_html = "<a href=\"https://www.boot.dev\">Boot Dev</a>"
+        self.assertEqual(node.to_leaf_node().to_html(), expected_html)
+
+    def test_tn_to_html_image(self):
+        node = TextNode("Boot Dev", TextType.IMAGE, "boot_dev.png")
+        expected_html = "<img src=\"boot_dev.png\" alt=\"Boot Dev\"></img>"
+        self.assertEqual(node.to_leaf_node().to_html(), expected_html)
 
 if __name__ == "__main__":
     unittest.main()
--- Static-Site-Generator/src/text_node.py
+++ Static-Site-Generator/src/text_node.py
@@ -1,4 +1,6 @@
+from typing import Optional, Union
 from enum import Enum
+from leaf_node import LeafNode
 
 
 class TextType(Enum):
@@ -9,30 +11,52 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
+TAG_TYPE = {
+    TextType.TEXT: None,  # no wrapping tag for plain text
+    TextType.BOLD: "b",
+    TextType.ITALIC: "i",
+    TextType.CODE: "pre",
+    TextType.LINK: "a",
+    TextType.IMAGE: "img",
+}
 
 class TextNode():
 
-    def __init__(self, text, text_type, url=None):
+    def __init__(self, text: str, text_type: TextType, props: Optional[Union[str, dict]] = None):
         '''
         Initializes a TextNode instance.
 
         Parameters:
             text (str): String of text.
             text_type (TextType): Defined enum value.
-            url (str, optional): Optional URL string. Defaults to None.
+            props (Union[str, dict], optional): Optional string or dictionary attributes. Defaults to None.
         '''
         self.text = text
         self.text_type = text_type
-        self.url = url
+        self.props = props
 
     # object string representation
     def __repr__(self):
-        url_display = f"\"{self.url}\"" if self.url is not None else None
-        return f"TextNode(text=\"{self.text}\", text_type={self.text_type}, url={url_display})"
+        return f"TextNode(text=\"{self.text}\", text_type={self.text_type}, props={self.props})"
 
     # equals
     def __eq__(self, other):
         equality = False
         if isinstance(other, TextNode):
-            equality = True if self.text == other.text and self.text_type == other.text_type and self.url == other.url else False
+            equality = True if self.text == other.text and self.text_type == other.text_type and self.props == other.props else False
         return equality
+    
+    def to_leaf_node(self):
+        special_value = [TextType.CODE, TextType.IMAGE]
+        special_props = [TextType.TEXT, TextType.CODE, TextType.LINK, TextType.IMAGE]
+        leaf_tag = TAG_TYPE.get(self.text_type, None)
+        leaf_value = self.text if self.text_type not in special_value else (
+            "" if self.text_type == TextType.IMAGE else
+            f"<code class=\"language-{self.props}\">{self.text}</code>"
+        )
+        leaf_props = self.props if self.text_type not in special_props else (
+            None if self.text_type == TextType.TEXT else
+            {"href":f"{self.props}"} if self.text_type == TextType.LINK else
+            {"src":f"{self.props}", "alt": f"{self.text}"}
+        )
+        return LeafNode(leaf_tag, leaf_value, leaf_props)
