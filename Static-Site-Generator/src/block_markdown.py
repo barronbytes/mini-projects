a38a1c8e95ec31e53c4dae22acd53d4b8ae7c9e6
barronbytes <jonathan.m.barron@gmail.com>
2025-02-15 20:20:26 -0500

    block_markdown: _create_patterns() method

--- Static-Site-Generator/src/block_markdown.py
+++ Static-Site-Generator/src/block_markdown.py
@@ -46,27 +46,27 @@ class BlockMarkdown():
         return equality
 
     def create_blocks(self) -> list[str]:
+        matches = self._create_patterns()
+        is_single_list = 1 == len(matches)
+        i_min_max = [0] if is_single_list else [0, len(matches)-1]
+        i_breakers = [i for i in range(len(matches)) if matches[i] == "\n"] if "\n" in matches else None
+        i_spans = [(i-1, i+1) for i in i_breakers] if (not is_single_list) and (i_breakers is not None) else None
+
+        i_spans_final = (
+            [
+                (i_spans[i][0], i_spans[i][1]) if (1 == len(i_spans)) or (i+1 == len(i_spans)) or (i_spans[i][1] != i_spans[i+1][0]) else 
+                (i_spans[i][0], i_spans[i+1][1])
+                for i in range(len(i_spans)) 
+                if i == 0 or i_spans[i][0] != i_spans[i-1][1]
+            ] if i_spans else None
+        )
+        return i_min_max, i_spans_final, matches
+
+    def _create_patterns(self) -> list[str]:
         regex = r"^\s*(.*?)\s*$|\n+"
         pattern = re.compile(pattern=regex, flags=re.DOTALL | re.MULTILINE)
         matches = list(pattern.findall(self.text))
-        lines = [m if m != "" else "\n" for m in matches]
-
-        is_one = 1 == len(lines)
-        i_min_max = [0] if is_one else [0, len(lines)-1]
-        i_breakers = [i for i in range(len(lines)) if lines[i] == "\n"] if "\n" in lines else None
-        i_spans = [(i-1, i+1) for i in i_breakers] if (not is_one) and (i_breakers is not None) else None
-        i_spans_final = []
-        if len(i_spans) is not None:
-            for i in range(len(i_spans)):
-                i_spans_final.append(
-                    (i_spans[i][0], i_spans[i][1])
-                )
-            #(i_spans[i][0], i_spans[i][1])
-            #(i_spans[i-1][0], i_spans[i][1]) if i > 0 and i_spans[i-1][1] == i_spans[i][0] else i_spans[i]
-            #for i in range(len(i_spans))
-            #if i == 0 or i_spans[i-1][1] != i_spans[i][0]
-        #] if i_spans is not None else None
-        return i_spans_final
+        return [m if m != "" else "\n" for m in matches]
 
     def to_blocks(self):
         blocks = self.create_blocks()
