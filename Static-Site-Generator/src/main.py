8c15f30e6ad4c4a19a5090333d789a3efc09b336
barronbytes <jonathan.m.barron@gmail.com>
2025-02-23 16:22:30 -0500

    main: refactored spacing

--- Static-Site-Generator/src/main.py
+++ Static-Site-Generator/src/main.py
@@ -2,10 +2,12 @@ from text_node import TextNode, TextType
 from copy_directory import CopyDirectory
 from create_page import CreatePage
 
+
 def main():
     brain = CopyDirectory("static", "public")
     brain.copy_parent_dir()
     CreatePage.create_pages()
+
 
 # run main() only when executed directly by main.py, not when imported by other files
 if __name__ == "__main__":
